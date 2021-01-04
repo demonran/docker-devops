eb1aae959e3ced2ec19acf9aaada7b408e083178
demonran <4445999306@qq.com>
2021-01-04 23:21:20 +0800

    删除无用日志

--- ddns/ddns.sh
+++ ddns/ddns.sh
@@ -4,4 +4,4 @@ current_path=$(cd "$(dirname $0)";pwd)
 
 cd $current_path
 
-pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple && python ddns.py
+pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple >/dev/nul 2>&1  && python ddns.py
